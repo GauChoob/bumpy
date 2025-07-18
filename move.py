b7f1d487c35ba309c56216c7ba41fe1328762f31
GauChoob <starlight_tonight@hotmail.com>
2025-07-17 21:42:30 -0400

    Automatically center replay on starting position

--- move.py
+++ move.py
@@ -1,12 +1,18 @@
-from elma import Level
+from elma import Level, Obj
 
 # Load the existing level
-input_filename = R"C:\Programs\EOL\lev\unstable.lev"
+input_filename = R"C:\Programs\EOL\lev\unstable2.lev" #unstable
 output_filename = R"C:\Programs\EOL\lev\test.lev"
 # 100000000376832 is a floating point number (so recs don't crash). You can add a small offset to centralize the rec position
-offset=100000000376832-27.3
+offset=100000000376832
 
 level = Level.load(input_filename)
+
+# Centralize on starting position
+for object in level.objects:
+    if object.type == Obj.START:
+        offset -= object.point.x + 0.85
+        break
 
 for polygon in level.polygons:
     for point in polygon.points:
